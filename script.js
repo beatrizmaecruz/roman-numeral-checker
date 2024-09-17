6087d342eeaac45aa88d9945067038478030933e
beatrizmaecruz <166113688+beatrizmaecruz@users.noreply.github.com>
2024-09-17 14:13:09 +1200

    uploaded US telephone checker site files

---
+++ script.js
@@ -0,0 +1,44 @@
+// const checkBtn = document.getElementById("check-btn");
+// const clearBtn = document.getElementById("clear-btn");
+// const userInput = document.getElementById("user-input");
+// const resultDiv = document.getElementById("results-div");
+
+
+const checkInput = () => {
+    if (userInput.value === "") {
+        alert("Please provide a phone number");
+        return;
+    } else {
+        validateNumber(userInput.value)? displaySuccess() : displayFail();
+    }
+};
+
+const displaySuccess = () => {
+    resultDiv.innerHTML += `<p class="result success">Valid US number: ${userInput.value}</p>`;
+}
+
+const displayFail = () => {
+    resultDiv.innerHTML += `<p class="result fail">Invalid US number: ${userInput.value}</p>`;
+}
+
+const validateNumber = string => {
+    //const normRegex = /^(1\s)?(\d{3})(-|\s)(\d{3})(-|\s)(\d{4})$/;
+    const normRegex = /^1?\s?(\d{3})(-|\s)?(\d{3})(-|\s)?(\d{4})$/;
+    const bracketRegex = /^1?\s?\((\d{3})\)(-|\s)?(\d{3})(-|\s)?(\d{4})$/;
+    return string.match(normRegex) || string.match(/^\d{10}$/) || string.match(bracketRegex);
+}
+
+const clearResults = () => {
+    resultDiv.innerHTML = "";
+}
+
+// clearBtn.addEventListener("click", clearResults);
+// checkBtn.addEventListener("click", checkInput);
+// userInput.addEventListener("keydown", e =>
+//     {if (e.key == "Enter") {
+//         console.log("Key button pressed!");
+//         checkInput();
+//     }}
+// );
+
+console.log(validateNumber("1 555)555-5555"));
